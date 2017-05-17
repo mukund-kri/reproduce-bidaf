5edcd4f5367fc36b352ff020018988b4f085c988
Raviteja973 <raviteja.p1234@gmail.com>
2017-05-17 15:37:41 +0530

    Add files via upload

--- slackbotDemo/bidaf.py
+++ slackbotDemo/bidaf.py
@@ -1,1 +1,65 @@
+import os
+import time
+import json
+from slackclient import SlackClient
+from rundemo import BidafBot
+
+with open('slackbot.config.json') as config:
+    slackbot_config = json.load(config)
+
+print(slackbot_config)
+
+# starterbot's ID as an environment variable
+#BOT_ID = os.environ.get("BOT_ID")
+BOT_ID = slackbot_config["bot_id"]
+
+# constants
+AT_BOT = "<@" + BOT_ID + ">"
+COMMAND = "getkab"
+
+# instantiate Slack & Twilio clients
+slack_client = SlackClient(slackbot_config["api_token"])
+
+bidafbot = BidafBot()
+
+
+def handle_command(command, channel):
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+    """
+    response = "Not sure what you mean. Use the *" + COMMAND
+    #print(command)
+    response = bidafbot.respond(command)
+    slack_client.api_call("chat.postMessage", channel=channel,text=response, as_user=True)
+
+
+def parse_slack_output(slack_rtm_output):
+    """
+        The Slack Real Time Messaging API is an events firehose.
+        this parsing function returns None unless a message is
+        directed at the Bot, based on its ID.
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                       output['channel']
+    return None, None
+
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("StarterBot connected and running!")
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
 
---
+++ slackbotDemo/rundemo.py
@@ -0,0 +1,31 @@
+from squad.demo_prepro import prepro
+from basic.demo_cli import Demo
+
+class BidafBot:
+        
+    def __init__(self):
+        self.paragraph = None
+        self.question = None
+        self.demo = Demo()
+	
+    #self.file = json.loads(open('bot_Q&A.json').read())
+    def getAnswer(self, paragraph, question):
+
+        pq_prepro = prepro(paragraph, question)
+        if len(pq_prepro['x'])>1000:
+            return "[Error] Sorry, the number of words in paragraph cannot be more than 1000." 
+        if len(pq_prepro['q'])>100:
+            return "[Error] Sorry, the number of words in question cannot be more than 100."
+        return self.demo.run(pq_prepro)
+
+    def respond(self,query):
+        if query.startswith("@context"):
+           self.paragraph = query.split("@context")[1]
+           answer = "Context received. Shoot your questions"
+        else :
+            self.question = query
+            answer = self.getAnswer(self.paragraph,self.question)
+        return answer
+
+#if __name__ == "__main__":
+#    main()
